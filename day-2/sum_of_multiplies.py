b991223f13ac18973bc2e6eac6fce573aa04a8ed
Tristanto <tristanto.kurniawan@gmail.com>
2020-04-20 11:07:05 +0700

    Refactor a bit

--- day-2/sum_of_multiplies.py
+++ day-2/sum_of_multiplies.py
@@ -1,5 +1,5 @@
-def sum_below(n: int) -> int:
-    return sum(filter(condition([3, 5]), range(1, n)))
+def sum_below(n: int, condition: callable) -> int:
+    return sum(filter(condition, range(1, n)))
 
 
 def condition(whitelist_numbers: list) -> int:
@@ -13,7 +13,8 @@ def condition(whitelist_numbers: list) -> int:
 
 
 if __name__ == '__main__':
-    print(sum_below(10))
-    assert 23 == sum_below(10)
-    print(sum_below(1000))
-    assert 233168 == sum_below(1000)
+    only_3_and_5_condition = condition([3, 5])
+    print(sum_below(10, only_3_and_5_condition))
+    assert 23 == sum_below(10, only_3_and_5_condition)
+    print(sum_below(1000, only_3_and_5_condition))
+    assert 233168 == sum_below(1000, only_3_and_5_condition)
