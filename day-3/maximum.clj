28bc5bddd394167e1f7ef6d0c62999d1cdda7d14
Tristanto <tristanto.kurniawan@gmail.com>
2020-04-23 08:00:47 +0700

    Use apply instead reduce

--- day-3/maximum.clj
+++ day-3/maximum.clj
@@ -3,9 +3,5 @@
     #(or (= 0 (mod % 3)) (= 0 (mod % 5)))
     (range limit)))
 
-(defn sum [array]
-  (reduce + array))
-
-
-(println (sum (generate-3-or-5 10)))
-(println (sum (generate-3-or-5 1000))) 
+(println (apply + (generate-3-or-5 10)))
+(println (apply + (generate-3-or-5 1000))) 
